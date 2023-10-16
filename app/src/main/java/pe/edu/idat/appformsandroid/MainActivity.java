f1097ad32b8a33239c231443ad30f91ce5820008
Dreyzen <jeffersonferre24@gmail.com>
2023-10-16 17:28:30 -0500

    inital commit

---
+++ app/src/main/java/pe/edu/idat/appformsandroid/MainActivity.java
@@ -0,0 +1,39 @@
+package pe.edu.idat.appformsandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import pe.edu.idat.appformsandroid.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnirlistado.setOnClickListener(this);
+        binding.btnirregistro.setOnClickListener(this);
+    }
+
+        @Override
+        public void onClick(View v) {
+
+            if(v.getId() == R.id.btnirlistado){
+                startActivity(new Intent(
+                        MainActivity.this, ListaActivity.class
+                ));
+            }else if (v.getId() == R.id.btnirregistro ){
+                startActivity(new Intent(
+                        MainActivity.this, ResgistroActivity.class
+                ));
+            }
+        }
+
+        }
